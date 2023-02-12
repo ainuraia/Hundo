d25113f3a2f9f713f43134d9f95f40fc2002a227
ainuraia <116380092+ainuraia@users.noreply.github.com>
2023-02-12 15:14:55 +0100

    JS functions

--- js/site.js
+++ js/site.js
@@ -1,5 +1,16 @@
-//Call Hello World function 
 
-function HelloWorld(){
-    alert("Hello World");
+
+// get the values from the Page
+function getValues(){
+
+}
+
+//generate numbers from the startValue to the endValue
+function generateNumbers(){
+
+}
+
+//display the numbers and mark the even ones bold
+function displayNumbers(){
+    
 }
