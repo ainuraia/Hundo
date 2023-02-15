ea131b663a3aae6377f8a24633b355b65c59f43c
ainuraia <116380092+ainuraia@users.noreply.github.com>
2023-02-15 19:34:20 +0100

    JS

--- js/site.js
+++ js/site.js
@@ -1,16 +1,28 @@
 
 
 // get the values from the Page
+//starts or controls function
 function getValues(){
+    //we get the values from the Page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    alert("The Start value:" + startValue) ;
+    //we call generateNumbers
+
+
+    //we call displayNumbers
 
 }
 
 //generate numbers from the startValue to the endValue
+// logic functions
 function generateNumbers(){
 
 }
 
 //display the numbers and mark the even ones bold
+// display or view functions
 function displayNumbers(){
     
 }
