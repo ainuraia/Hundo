b6929f343ea37f98fde558b19cd169b181a3abe2
ainuraia <116380092+ainuraia@users.noreply.github.com>
2023-02-17 17:15:40 +0100

    Added functions

--- js/site.js
+++ js/site.js
@@ -7,18 +7,34 @@ function getValues(){
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
 
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue)
+
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        
+    }
+
     alert("The Start value:" + startValue);
     //we call generateNumbers
 
-
+    let numbers = generateNumbers(startValue, endValue);
     //we call displayNumbers
 
 }
 
 //generate numbers from the startValue to the endValue
 // logic functions
-function generateNumbers(){
+function generateNumbers(sValue, eValue){
+    let numbers = [];
 
+    // we want to get all numbers from start to end
+    for (let index = sValue; index <= eValue; index++) {
+        ;
+        
+        //this will execute in a loop until index = eValue
+        numbers.push(index)
+    }    
+    return numbers ;
 }
 
 //display the numbers and mark the even ones bold
