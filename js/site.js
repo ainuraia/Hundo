8e8028cd1902e0822f4d950febf366c9c9c7f127
ainuraia <116380092+ainuraia@users.noreply.github.com>
2023-02-20 19:54:53 +0100

    displayNumbers needs fixing

--- js/site.js
+++ js/site.js
@@ -6,23 +6,27 @@ function getValues(){
     //we get the values from the Page
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
+   
 
     startValue = parseInt(startValue);
-    endValue = parseInt(endValue)
+    endValue = parseInt(endValue);
 
     if(Number.isInteger(startValue) && Number.isInteger(endValue)) {
         //we call generateNumbers
         let numbers = generateNumbers(startValue, endValue);
+       
+        displayNumbers(numbers);
+
     }else{
         alert("You must enter integers");
     }
 
-    alert("The Start value:" + startValue);
+  //  alert("The Start value:" + startValue);
     //we call generateNumbers
 
-    let numbers = generateNumbers(startValue, endValue);
+    //let numbers = generateNumbers(startValue, endValue);
     //we call displayNumbers
-
+    
 }
 
 //generate numbers from the startValue to the endValue
@@ -32,16 +36,31 @@ function generateNumbers(sValue, eValue){
 
     // we want to get all numbers from start to end
     for (let index = sValue; index <= eValue; index++) {
-        ;
-        
-        //this will execute in a loop until index = eValue
-        numbers.push(index)
+         //this will execute in a loop until index = eValue
+        numbers.push(index);
     }    
+   
     return numbers ;
 }
 
 //display the numbers and mark the even ones bold
 // display or view functions
-function displayNumbers(){
+function displayNumbers(numbers){
     
+    let templateRows = "";
+    for (let index = 0; index < numbers.length; index++) {
+       
+        let className = "even";
+        let number = numbers[index];
+        if (number % 2 == 0){
+            className = "even";
+        }
+        else{
+            className = "odd";
+        }
+       
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+    }
+
+    documents.getElementById("results").innerHTML = templateRows;
 }
